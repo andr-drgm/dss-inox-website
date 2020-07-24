f348bedd63db5d4b008660677ed230b554177a9d
andr-drgm <dragomirandy900@gmail.com>
2020-07-24 11:29:34 +0300

    contact page

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
     });
 
     new Rellax(rellaxRef3.current, { // <---- Via useRef element
-      speed: 5,
+      speed: -2,
       center: true,
       wrapper: null,
       round: true,
@@ -46,49 +46,70 @@ function App() {
       horizontal: false
     });
 
-  },[]);
+  }, []);
 
   return (
     <div className="container">
 
       <div className="landingPage">
-        <div className="landingPageText" style={{color: "white"}}>
+        <div className="landingPageText" style={{ color: "white" }}>
           <div className="landingTitle">DSS-INOX</div>
           <div className="description">Confecții din oțel inoxidabil</div>
         </div>
-        <img className="landingPageImage" ref={rellaxRef} src={require("./images/bara-in-house.jpg")} alt="Bara de inox"></img>
+        <div className="landingPageImage" ref={rellaxRef} src={require("./images/bara-in-house.jpg")} alt="Bara de inox"></div>
       </div>
 
       <div className="page">
-        <img className="page2Image" ref={rellaxRef2} src={require("./images/welder2.jpg")} alt="Page 2"></img>
+        <div className="page2Image" ref={rellaxRef2}></div>
         <div className="landingPageText">
           <div className="title">In ritm cu tehnologia</div>
           <div className="description">Folosim cele mai noi tehnogii
-pentru a oferii cea mai buna 
+          pentru a oferii cea mai buna
 experiență clienților noștrii</div>
         </div>
       </div>
 
-      <div className="page" style={{backgroundColor: "black"}}>
+      <div className="page" style={{ backgroundColor: "black" }}>
         <div className="landingPageText">
-          <div className="title" style={{color: "white"}}>Durabilitate</div>
-          <div className="description" style={{color: "white"}}>Te vei bucura toată viața
-de un produs care își
-păstrează propietațile
+          <div className="title" style={{ color: "white" }}>Durabilitate</div>
+          <div className="description" style={{ color: "white" }}>Te vei bucura toată viața
+          de un produs care își
+          păstrează propietațile
 și aspectul în timp</div>
         </div>
-        <img className="page3Image" ref={rellaxRef3} src={require("./images/bara.jpg")} alt="Page 2"></img>
+        <div className="page3Image" ref={rellaxRef3}></div>
       </div>
 
       <div className="page">
-        <img className="page2Image" ref={rellaxRef4} src={require("./images/stairs.jpg")} alt="Page 2"></img>
+        <div className="page4Image" ref={rellaxRef4}></div>
         <div className="landingPageText">
           <div className="title">Modern</div>
           <div className="description">Împrospătează aspectul
-locuinței tale cu un aer
-modern oferit de decorațiunile
+          locuinței tale cu un aer
+          modern oferit de decorațiunile
 din oțel inoxidabil</div>
         </div>
+      </div>
+
+      <div className="pageContact" style={{ backgroundColor: "black" }}>
+        <div className="contactData" style={{ color: "white" }}> <p style={{fontSize: 30}}>Contact</p>
+        <div className="dataPair">
+            <div className="locationIcon"></div>
+            <a className="dataText" href="https://www.google.com/maps/place/Media%C8%99/" style={{ color: "white", textDecoration: "none" }}>Mediaș, Sibiu</a>
+          </div>
+          <div className="dataPair">
+            <div className="emailIcon"></div>
+            <a className="dataText" href="mailto:dragomirstefan74@gmail.com" style={{ color: "white", textDecoration: "none" }}>dragomirstefan74@gmail.com</a>
+          </div>
+          <div className="dataPair">
+            <div className="phoneIcon"></div>
+            <a className="dataText" href="tel:+40744663034" style={{ color: "white", textDecoration: "none" }}>+40 744 663 034</a>
+          </div>
+        </div>
+      </div>
+      <div className="siteCreator" style={{ backgroundColor: "black", color: "white" }}
+        >© Copyright 2020 DSS-INOX. Website created by <a style={{ color: "white" }} href="http://andr-drgm.github.io/"
+        >Andrei Dragomir</a>
       </div>
     </div>
   );
